f6d7cb101f9f93260e951630cfffa0df5aefa2a1
vinsent2024 <168044144+vinsent2024@users.noreply.github.com>
2024-07-12 11:11:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,5 @@
 import "./assets/css/tailwind.css";
 
-// Check if dark mode is already set in localStorage or by system preference
-if (
-    localStorage.getItem("color-theme") === "dark" ||
-    (!localStorage.getItem("color-theme") &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-    document.documentElement.classList.add("dark"); // Apply dark mode class to the root element
-    localStorage.setItem("color-theme", "dark"); // Store dark mode preference
-} else {
-    // If light mode is detected, force dark mode
-    document.documentElement.classList.add("dark");
-    localStorage.setItem("color-theme", "dark");
-}
-
-let switchers = document.querySelectorAll(".switcher");
-
-switchers.forEach((switcher) => {
-    switcher.addEventListener("click", function () {
-        // Toggle dark mode
-        if (document.documentElement.classList.contains("dark")) {
-            document.documentElement.classList.remove("light");
-            localStorage.setItem("color-theme", "dark");
-        } else {
-            document.documentElement.classList.add("dark");
-            localStorage.setItem("color-theme", "dark");
-        }
-    });
-});
 
 
 
